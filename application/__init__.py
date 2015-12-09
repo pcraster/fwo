664b5ce731615d11135e8eba77ad91fa4ab94814
Koko Alberti <kokoalberti@fastmail.nl>
2015-12-09 16:24:43 +0100

    removed unused files and moved a few others

--- application/__init__.py
+++ application/__init__.py
@@ -1,36 +1,21 @@
-#from flask import Flask, render_template, request, redirect, abort, flash, send_file, send_from_directory, url_for, make_response, Response
-
-
 from flask import Flask
-#from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.user import UserManager, UserMixin, SQLAlchemyAdapter
-#from flask.ext.user import current_user, login_required, roles_required, UserManager, UserMixin, SQLAlchemyAdapter
-# from slugify import slugify
 
-
+#Create Flask app
 app = Flask(__name__)
 
+#Load the configuration
 app.config.from_object("application.settings")
 
-# Load local_settings.py if file exists
+#Load private config from local_settings.py
 try: app.config.from_object('application.local_settings')
 except: pass
 
-#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+#Load SQLAlachemy extension
+db = SQLAlchemy(app)
 
-db=SQLAlchemy(app)
-
-
+#Import views and models
 from application import views, models
-
-
-#db.create_all()
-# Setup Flask-User
-
-
-#from views import *
-
 
 if __name__ == "__main__":
 	app.run(debug=True)
